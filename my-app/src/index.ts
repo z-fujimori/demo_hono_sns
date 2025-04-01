1bc5ba2353f88f8ddd88e03276c0fd5c12e21ec9
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2025-04-01 22:07:36 +0900

    hello world

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -2,6 +2,11 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+Bun.serve({
+  fetch: app.fetch,
+  port: Number(process.env.PORT || 3000),
+})
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
