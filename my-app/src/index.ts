476fadb629545d3184815dace6cb1494bc21aae3
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2025-04-03 08:09:42 +0900

    リクエストパラメータ取得

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -11,4 +11,27 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.get('/hello', (c) => {
+  return c.text('hello')
+})
+
+app.post('/post', (c) => {
+  return c.text('post')
+})
+
+app.get('/search', async (c) => {
+  const { q, limit, offset } = c.req.query()
+  if (q) {
+    console.log("q: ",q)
+  }
+  if (limit) {
+    console.log("limit: ",limit)
+  }
+  if (offset) {
+    console.log("offset: ",offset)
+  }
+
+  return c.text("logに表示")
+})
+
 export default app
